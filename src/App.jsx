3a47e4980fb15160ce246772294b1da01f27e003
Mecshi <maxi.correab@gmail.com>
2023-02-10 12:27:27 -0300

    add hocks and cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,43 @@
+import { useEffect, useState } from 'react'
 function App () {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    // cleanup
+    // --> se ejecuta cuando el componente App se desmonta
+    // --> se ejecuta cuando las dependencias del useEffect cambian (enabled o !enabled), antes de ejecutar el efecto nuevamente
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
   return (
-    <h1>Proyect 3</h1>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px,${position.y}px)`
+      }}
+      />
+      <button
+        onClick={() => setEnabled(!enabled)}
+      >
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
   )
 }
 
