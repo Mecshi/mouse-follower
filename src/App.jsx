8795b144beaa31e7d8d095d61ffd20d3fa972a8f
Mecshi <maxi.correab@gmail.com>
2023-02-10 13:14:39 -0300

    change bg and pointer position

--- src/App.jsx
+++ src/App.jsx
@@ -29,10 +29,10 @@ const FollowMouse = () => {
         borderRadius: '50%',
         opacity: 0.8,
         pointerEvents: 'none',
-        left: -20,
-        top: -20,
-        width: 40,
-        height: 40,
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
         transform: `translate(${position.x}px,${position.y}px)`
       }}
       />
